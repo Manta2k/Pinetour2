153e434f15520c9d01acedef4bd7f2b3d0d1758f
Bayr-Erdene Enkhtur <23LP1331@G301-CC0132.local>
2024-01-17 19:59:55 +0800

    1.17

---
+++ portfolio/rata/src/component/intro.jsx
@@ -0,0 +1,13 @@
+export const Intro = () => {
+  return (
+    <>
+      <div className="flex justify-end group *:mr-[30px] *:text-[20px] mt-[20px]">
+        <div className="">About</div>
+        <div className="">Work</div>
+        <div className="">Testimonials</div>
+        <div className="">Contact</div>
+      </div>
+      <h1 className="text-[60px] font-bold"> HI Bayar-Erdene</h1>
+    </>
+  );
+};
